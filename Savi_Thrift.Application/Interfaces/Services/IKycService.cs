6d07e983449a677042350264688d6c26c565c563
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-10 11:00:46 +0100

    AddKycService

--- Savi_Thrift.Application/Interfaces/Services/IKycService.cs
+++ Savi_Thrift.Application/Interfaces/Services/IKycService.cs
@@ -12,29 +12,12 @@ namespace Savi_Thrift.Application.Interfaces.Services
 {
     public interface IKycService
     {
-        //Task<ApiResponse<KycRequestDto>> GetKycByIdAsync(string id);
-        //Task<ApiResponse<GetAllKycsDto>> GetKycsByPaginationAsync(int page, int perPage);
-        //Task<ApiResponse<KycResponseDto>> AddKycAsync(string userId, KycRequestDto kycDto);
-        //Task<ApiResponse<KycResponseDto>> UpdateKycAsync(string kycId, UpdateKycDto updateKycDto);
-        //Task<ApiResponse<bool>> DeleteKycByIdAsync(string kycId);
-
-
-        //Task<ApiResponse<KycResponseDto>> AddKyc(KycRequestDto kycRequest);
-        //Task<ApiResponse<bool>> DeleteKyc(string kycId);
-        //Task<ApiResponse<PageResult<KycResponseDto>>> GetAllKycs(int page, int perPage);
-        //Task<ApiResponse<KycResponseDto>> GetKycById(string kycId);
-        //Task<ApiResponse<bool>> UpdateKyc(string kycId, KycRequestDto updatedKyc);
-        //Task<ApiResponse<string>> UploadIdentificationDocument(string kycId, IFormFile file);
-        //Task<ApiResponse<string>> UploadProofOfAddressDocument(string kycId, IFormFile file);
-
-        Task<ApiResponse<KycResponseDto>> AddKyc(KycRequestDto kycRequest);
+        Task<ApiResponse<KycResponseDto>> AddKyc(string userId, KycRequestDto kycDto);
         Task<ApiResponse<bool>> DeleteKyc(string kycId);
-        Task<ApiResponse<PageResult<KycResponseDto>>> GetAllKycs(int page, int perPage);
+        Task<ApiResponse<PageResult<IEnumerable<KycResponseDto>>>> GetAllKycs(int page, int perPage);
         Task<ApiResponse<KycResponseDto>> GetKycById(string kycId);
         Task<ApiResponse<bool>> UpdateKyc(string kycId, KycRequestDto kycRequest);
         Task<ApiResponse<CloudinaryUploadResponse>> UploadIdentificationDocument(string kycId, IFormFile file);
         Task<ApiResponse<CloudinaryUploadResponse>> UploadProofOfAddressDocument(string kycId, IFormFile file);
-
-
     }
 }
---
+++ Savi_Thrift.Application/ServicesImplementation/KycService.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.Crypto;
+using Savi_Thrift.Application.DTO;
+using Savi_Thrift.Application.Interfaces.Repositories;
+using Savi_Thrift.Application.Interfaces.Services;
+using Savi_Thrift.Common.Utilities;
+using Savi_Thrift.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketEase.Domain;
+
+namespace Savi_Thrift.Application.ServicesImplementation
+{
+    public class KycService : IKycService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<KycService> _logger;
+        private readonly ICloudinaryServices _cloudinaryServices;
+
+        public KycService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<KycService> logger, ICloudinaryServices cloudinaryServices)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+            _cloudinaryServices = cloudinaryServices;
+        }
+
+        public async Task<ApiResponse<KycResponseDto>> AddKyc(string userId, KycRequestDto kycDto)
+        {
+            try
+            {
+                var existingKyc = await _unitOfWork.KycRepository.GetKycByIdAsync(userId);
+                if (existingKyc != null)
+                {
+                    return ApiResponse<KycResponseDto>.Failed(false, "KYC already exists for the user", StatusCodes.Status400BadRequest, new List<string>());
+                }
+                var newKyc = _mapper.Map<KYC>(kycDto);
+                await _unitOfWork.KycRepository.AddKycAsync(newKyc);
+                _unitOfWork.SaveChanges();
+                var addedKycDto = _mapper.Map<KycResponseDto>(newKyc);
+                return ApiResponse<KycResponseDto>.Success(addedKycDto, "KYC added successfully.", StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while adding KYC");
+                return new ApiResponse<KycResponseDto>(false, "Error occurred while processing your request", StatusCodes.Status500InternalServerError, new List<string> { ex.Message });
+            }
+        }
+
+        public Task<ApiResponse<bool>> DeleteKyc(string kycId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<PageResult<IEnumerable<KycResponseDto>>>> GetAllKycs(int page, int perPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<KycResponseDto>> GetKycById(string kycId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<bool>> UpdateKyc(string kycId, KycRequestDto kycRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<CloudinaryUploadResponse>> UploadIdentificationDocument(string kycId, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<CloudinaryUploadResponse>> UploadProofOfAddressDocument(string kycId, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
