8cd6af4835b7ff3366b0e977c59db0ad65c64129
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-10 10:16:15 +0100

    MapperProfile

--- Savi_Thrift.Application/Interfaces/Services/IKycService.cs
+++ Savi_Thrift.Application/Interfaces/Services/IKycService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Savi_Thrift.Application.DTO;
+using Savi_Thrift.Common.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +12,29 @@ namespace Savi_Thrift.Application.Interfaces.Services
 {
     public interface IKycService
     {
-        Task<ApiResponse<KycRequestDto>> GetKycByIdAsync(string id);
-        Task<ApiResponse<GetAllKycsDto>> GetKycsByPaginationAsync(int page, int perPage);
-        Task<ApiResponse<KycResponseDto>> AddKycAsync(string userId, KycRequestDto kycDto);
-        Task<ApiResponse<KycResponseDto>> UpdateKycAsync(string kycId, UpdateKycDto updateKycDto);
-        Task<ApiResponse<bool>> DeleteKycByIdAsync(string kycId);
+        //Task<ApiResponse<KycRequestDto>> GetKycByIdAsync(string id);
+        //Task<ApiResponse<GetAllKycsDto>> GetKycsByPaginationAsync(int page, int perPage);
+        //Task<ApiResponse<KycResponseDto>> AddKycAsync(string userId, KycRequestDto kycDto);
+        //Task<ApiResponse<KycResponseDto>> UpdateKycAsync(string kycId, UpdateKycDto updateKycDto);
+        //Task<ApiResponse<bool>> DeleteKycByIdAsync(string kycId);
+
+
+        //Task<ApiResponse<KycResponseDto>> AddKyc(KycRequestDto kycRequest);
+        //Task<ApiResponse<bool>> DeleteKyc(string kycId);
+        //Task<ApiResponse<PageResult<KycResponseDto>>> GetAllKycs(int page, int perPage);
+        //Task<ApiResponse<KycResponseDto>> GetKycById(string kycId);
+        //Task<ApiResponse<bool>> UpdateKyc(string kycId, KycRequestDto updatedKyc);
+        //Task<ApiResponse<string>> UploadIdentificationDocument(string kycId, IFormFile file);
+        //Task<ApiResponse<string>> UploadProofOfAddressDocument(string kycId, IFormFile file);
+
+        Task<ApiResponse<KycResponseDto>> AddKyc(KycRequestDto kycRequest);
+        Task<ApiResponse<bool>> DeleteKyc(string kycId);
+        Task<ApiResponse<PageResult<KycResponseDto>>> GetAllKycs(int page, int perPage);
+        Task<ApiResponse<KycResponseDto>> GetKycById(string kycId);
+        Task<ApiResponse<bool>> UpdateKyc(string kycId, KycRequestDto kycRequest);
+        Task<ApiResponse<CloudinaryUploadResponse>> UploadIdentificationDocument(string kycId, IFormFile file);
+        Task<ApiResponse<CloudinaryUploadResponse>> UploadProofOfAddressDocument(string kycId, IFormFile file);
+
+
     }
 }
--- Savi_Thrift/Mapper/MapperProfile.cs
+++ Savi_Thrift/Mapper/MapperProfile.cs
@@ -1,8 +1,15 @@
 ﻿using AutoMapper;
+using Savi_Thrift.Application.DTO;
+using Savi_Thrift.Domain.Entities;
 
 namespace Savi_Thrift.Mapper
 {
     public class MapperProfile : Profile
     {
+        public MapperProfile()
+        {
+            CreateMap<KycRequestDto, KYC>().ReverseMap();
+            CreateMap<KYC, KycResponseDto>().ReverseMap();
+        }
     }
 }
