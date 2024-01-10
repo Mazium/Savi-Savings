e32a82ce102156f1f0084ce1f5e8a647fdd26430
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-10 08:32:35 +0100

    IKYCRepository

---
+++ Savi_Thrift.Application/Interfaces/Services/IKycService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketEase.Domain;
+
+namespace Savi_Thrift.Application.Interfaces.Services
+{
+    public interface IKycService
+    {
+        Task<ApiResponse<KycRequestDto>> GetKycByIdAsync(string id);
+        Task<ApiResponse<GetAllKycsDto>> GetKycsByPaginationAsync(int page, int perPage);
+        Task<ApiResponse<KycResponseDto>> AddKycAsync(string userId, KycRequestDto kycDto);
+        Task<ApiResponse<KycResponseDto>> UpdateKycAsync(string kycId, UpdateKycDto updateKycDto);
+        Task<ApiResponse<bool>> DeleteKycByIdAsync(string kycId);
+    }
+}
---
+++ Savi_Thrift/Mapper/MapperProfile.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace Savi_Thrift.Mapper
+{
+    public class MapperProfile : Profile
+    {
+    }
+}
