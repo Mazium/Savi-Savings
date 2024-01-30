53371c4b11041a780373f6bcadb0b191ac4594fc
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-30 20:27:04 +0100

    UpdatedWalletEntities

---
+++ Savi_Thrift.Application/Interfaces/Services/IPaystackService.cs
@@ -0,0 +1,8 @@
+﻿//namespace Savi_Thrift.Application.Interfaces.Services
+//{
+//    public interface IPaystackService
+//    {
+//        Task<string> InitializePayment(decimal amount, string email, string reference);
+//        Task<bool> VerifyPayment(string reference);
+//    }
+//}
